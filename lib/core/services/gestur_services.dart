bb6e71ee4b602339f5701b8c60625ad9cc000cbb
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2024-10-15 15:44:03 +0700

    feat: change url to azure backend services

--- lib/core/services/gestur_services.dart
+++ lib/core/services/gestur_services.dart
@@ -3,7 +3,7 @@ import 'package:temanbisindoa9/core/models/gestur_models.dart';
 
 class GesturService {
 // final String baseUrl = 'http://10.0.2.2:8080/';
-  final String baseUrl = 'https://66b4-112-78-132-212.ngrok-free.app/';
+  final String baseUrl = 'https://capstone-a9db.azurewebsites.net';
 
   Future<List<GesturModel>> getAllGestur() async {
     final response = await http.get(Uri.parse('$baseUrl/get_all_gestur'));
